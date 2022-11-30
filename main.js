355ae8e9c3d5353c8da0ab847827a26df6f2074f
Hafsafathima26 <78514176+Hafsafathima26@users.noreply.github.com>
2022-11-29 19:01:22 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup(){
+    canvas=createCanvas(550,550);
+    canvas.position(700,150);
+}
+
+function draw(){
+    fill(255, 0, 0);
+    stroke(255, 0, 0); 
+    circle(520,30,80);
+
+    fill(255, 0, 0);
+    stroke(255, 0, 0); 
+    circle(30,30,80);
+
+    fill(255, 0, 0);
+    stroke(255, 0, 0); 
+    circle(30,520,80);
+
+    fill(255, 0, 0);
+    stroke(255, 0, 0); 
+    circle(520,520,80);
+
+    
+    
+}
